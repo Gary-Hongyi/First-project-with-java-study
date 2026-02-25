760ad909fb7287578cd44560750625b227ca484c
Gary <aviwowoyos059@gmail.com>
2026-02-25 14:20:34 +0800

    Learn java first commit!

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("\"I love pizza\"\n");
+        System.out.println("It's really good!");
+        //This is an comment!
+//        /*hello
+//        men
+//         */
+    }
+}
