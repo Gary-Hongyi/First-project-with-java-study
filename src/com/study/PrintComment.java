1f66ffe8be76cef34eb3013aaf8a272a79c409aa
Gary <aviwowoyos059@gmail.com>
2026-02-25 14:21:07 +0800

    Learn java day1 commit!

---
+++ src/com/study/PrintComment.java
@@ -0,0 +1,15 @@
+package com.study;
+
+public class PrintComment {
+    public static void main(String[] args){
+
+        System.out.println("\"I love pizza\"\n");
+        System.out.println("It's really good!");
+        //This is an comment!
+//        /*hello
+//        men
+//         */
+    }
+
+
+}
